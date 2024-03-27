5ad47df728bebb229199a0e367c215a2a4aa2e6f
Jorge Martin <jorgemctin@gmail.com>
2024-03-27 20:02:18 +0100

    Feat: ApiCall to bring users

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+interface User {
+  id: number;
+  email: string;
+  first_name: string;
+  last_name: string;
+  avatar: string;
+}
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
+  users: User[] = [];
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get<any>('https://reqres.in/api/users?page=1').subscribe(response => {
+      this.users = response.data; // Asignamos el array de usuarios
+    });
+  }
 }
