ad7d7a1a6ecbf7523d6e63325ad27a28d49e827c
Jorge Martin <jorgemctin@gmail.com>
2024-03-28 15:36:45 +0100

    Feat: pagination in Users component

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 import { CommonModule } from '@angular/common';
+
 interface User {
   id: number;
   email: string;
@@ -17,12 +19,35 @@ interface User {
 })
 export class UsersComponent implements OnInit {
   users: User[] = [];
+  currentPage: number = 1;
+  hasNextPage: boolean = true;
+  hasPreviousPage: boolean = false;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.http.get<any>('https://reqres.in/api/users?page=1').subscribe(response => {
-      this.users = response.data; // Asignamos el array de usuarios
+    this.loadUsers(this.currentPage);
+  }
+
+  loadUsers(pageNumber: number): void {
+    this.http.get<{ data: User[]; page: number; total_pages: number }>(`https://reqres.in/api/users?page=${pageNumber}`).pipe(
+      map(response => {
+        this.hasNextPage = response.page < response.total_pages;
+        this.hasPreviousPage = response.page > 1;
+        return response.data;
+      })
+    ).subscribe(users => {
+      this.users = users;
     });
+  }
+
+  loadNextPage(): void {
+    this.currentPage++;
+    this.loadUsers(this.currentPage);
+  }
+
+  loadPreviousPage(): void {
+    this.currentPage--;
+    this.loadUsers(this.currentPage);
   }
 }
