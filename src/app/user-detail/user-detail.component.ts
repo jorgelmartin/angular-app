798f96fcf7186a03e02d56296707d9cd6cc59b55
Jorge Martin <jorgemctin@gmail.com>
2024-03-28 17:32:49 +0100

    Feat: ApiCall to show userDetail

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+interface UserData {
+  id: number;
+  email: string;
+  first_name: string;
+  last_name: string;
+  avatar: string;
+}
 
 @Component({
   selector: 'app-user-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-detail.component.html',
   styleUrl: './user-detail.component.css'
 })
-export class UserDetailComponent {
 
+export class UserDetailComponent implements OnInit {
+  user!: UserData;
+
+  constructor(
+    private route: ActivatedRoute,
+    private http: HttpClient,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      const userId = params['id'];
+      this.loadUserDetail(userId);
+    });
+  }
+
+  loadUserDetail(userId: number): void {
+    this.http.get<{ data: UserData }>(`https://reqres.in/api/users/${userId}`).subscribe(response => {
+      this.user = response.data;
+    });
+  }
+
+  goToUsersPage(): void {
+    this.router.navigate(['/users']);
+  }
 }
